0fca15d4aff0be000b95c914679492bc8f164fe6
Shuo Kong <shuo.kong@yale.edu>
2018-06-05 17:41:13 -0400

    mass-vdiff

--- Lane2016/show_vdiff.py
+++ Lane2016/show_vdiff.py
@@ -28,8 +28,10 @@ xpanels = 1
 ypanels = len(linenames)
 xpanelwidth = 12
 ypanelwidth = 5
-vlow = 0
-vhigh = 16
+vlow = -8
+vhigh = 8
+mlow = np.nanmin(coremasses)
+mhigh = np.nanmax(coremasses)
 cols = len(corenames)
 
 usepeakvel = 0
@@ -42,10 +44,10 @@ if usepeakvel == 1:
             panel = i+j*xpanels+1
             print 'panel',panel 
             coreveldiff = diffvelocities[panel-1]
-            hist, bin_edges = np.histogram(coreveldiff,bins='auto',range=(-8,8))
+            hist, bin_edges = np.histogram(coreveldiff,bins='auto',range=(vlow,vhigh))
             bincenter = (bin_edges[:-1] + bin_edges[1:]) / 2.
             popt,pcov = curve_fit(gaus,bincenter,hist,p0=[1,0,0.5])
-            datafiles['panel'+str(panel)] = {'title':linenames[j],'lines':{'1':{'x':bincenter,'y':hist,'velocity':coreveldiff,'peaksnr':[],'legends':'data','linestyles':'k-','drawsty':'steps-mid'},'2':{'x':bincenter,'y':gaus(bincenter,*popt),'velocity':coreveldiff,'peaksnr':[],'legends':'fit','linestyles':'b-','drawsty':'default'}},'xlim':[-8,8],'ylim':[0,np.nanmax(hist)*1.1],'xscale':'linear','yscale':'linear','xlabel':r'$v_{\rm diff}~\rm (km~s^{-1})$','ylabel':r'$\rm number$','text':'','vertlines':[]}
+            datafiles['panel'+str(panel)] = {'title':linenames[j],'lines':{'1':{'x':bincenter,'y':hist,'velocity':coreveldiff,'peaksnr':[],'legends':'data','linestyles':'k-','drawsty':'steps-mid'},'2':{'x':bincenter,'y':gaus(bincenter,*popt),'velocity':coreveldiff,'peaksnr':[],'legends':'fit','linestyles':'b-','drawsty':'default'}},'xlim':[vlow,vhigh],'ylim':[0,np.nanmax(hist)*1.1],'xscale':'linear','yscale':'linear','xlabel':r'$v_{\rm diff}~\rm (km~s^{-1})$','ylabel':r'$\rm number$','text':'','vertlines':[]}
     
     fig=plt.figure(figsize=(xpanelwidth*xpanels*1.1,ypanelwidth*ypanels/1.1))
     plt.subplots_adjust(wspace=0.001,hspace=0.001)
@@ -108,7 +110,7 @@ if usepeakvel == 1:
     os.system('open '+pdfname)
     os.system('cp '+pdfname+os.path.expandvars(' ~/GoogleDrive/imagesSFE/'))
 
-usemom1vel = 1
+usemom1vel = 0
 if usemom1vel == 1:
     diffvelocities = [nh3velocities-coremom1_12CO,nh3velocities-coremom1_13CO,nh3velocities-coremom1_C18O]
     datafiles = {}
@@ -118,10 +120,10 @@ if usemom1vel == 1:
             panel = i+j*xpanels+1
             print 'panel',panel 
             coreveldiff = diffvelocities[panel-1]
-            hist, bin_edges = np.histogram(coreveldiff,bins='auto',range=(-8,8))
+            hist, bin_edges = np.histogram(coreveldiff,bins='auto',range=(vlow,vhigh))
             bincenter = (bin_edges[:-1] + bin_edges[1:]) / 2.
             popt,pcov = curve_fit(gaus,bincenter,hist,p0=[1,0,0.5])
-            datafiles['panel'+str(panel)] = {'title':linenames[j],'lines':{'1':{'x':bincenter,'y':hist,'velocity':coreveldiff,'peaksnr':[],'legends':'data','linestyles':'k-','drawsty':'steps-mid'},'2':{'x':bincenter,'y':gaus(bincenter,*popt),'velocity':coreveldiff,'peaksnr':[],'legends':'fit','linestyles':'b-','drawsty':'default'}},'xlim':[-8,8],'ylim':[0,np.nanmax(hist)*1.1],'xscale':'linear','yscale':'linear','xlabel':r'$v_{\rm diff}~\rm (km~s^{-1})$','ylabel':r'$\rm number$','text':'','vertlines':[]}
+            datafiles['panel'+str(panel)] = {'title':linenames[j],'lines':{'1':{'x':bincenter,'y':hist,'velocity':coreveldiff,'peaksnr':[],'legends':'data','linestyles':'k-','drawsty':'steps-mid'},'2':{'x':bincenter,'y':gaus(bincenter,*popt),'velocity':coreveldiff,'peaksnr':[],'legends':'fit','linestyles':'b-','drawsty':'default'}},'xlim':[vlow,vhigh],'ylim':[0,np.nanmax(hist)*1.1],'xscale':'linear','yscale':'linear','xlabel':r'$v_{\rm diff}~\rm (km~s^{-1})$','ylabel':r'$\rm number$','text':'','vertlines':[]}
     
     fig=plt.figure(figsize=(xpanelwidth*xpanels*1.1,ypanelwidth*ypanels/1.1))
     plt.subplots_adjust(wspace=0.001,hspace=0.001)
@@ -184,5 +186,151 @@ if usemom1vel == 1:
     os.system('open '+pdfname)
     os.system('cp '+pdfname+os.path.expandvars(' ~/GoogleDrive/imagesSFE/'))
 
+massvel = 1
+if massvel == 1:
+    diffvelocities = [nh3velocities-corevelocities12CO,nh3velocities-corevelocities13CO,nh3velocities-corevelocitiesC18O]
+    datafiles = {}
+    for i in range(0,xpanels):
+        for j in range(0,ypanels):
+            print linenames[j]
+            panel = i+j*xpanels+1
+            print 'panel',panel 
+            coreveldiff = diffvelocities[panel-1]
+            datafiles['panel'+str(panel)] = {'title':linenames[j],'lines':{'1':{'x':coreveldiff,'y':coremasses,'velocity':coreveldiff,'peaksnr':[],'legends':'data','linestyles':'ko','drawsty':'default'},},'xlim':[vlow,vhigh],'ylim':[mlow,mhigh],'xscale':'linear','yscale':'linear','xlabel':r'$v_{\rm diff}~\rm (km~s^{-1})$','ylabel':r'$\rm mass$','text':'','vertlines':[]}
+    
+    fig=plt.figure(figsize=(xpanelwidth*xpanels*1.1,ypanelwidth*ypanels/1.1))
+    plt.subplots_adjust(wspace=0.001,hspace=0.001)
+    pdfname='corespectra/vdiffmass.pdf'
+    for i in range(0,xpanels):
+        for j in range(0,ypanels):
+            panelnum = i+j*xpanels+1
+            ax = fig.add_subplot(ypanels,xpanels,panelnum)
+            if 'panel'+str(panelnum) not in datafiles.keys(): continue
+            ax.set_xscale(datafiles['panel'+str(panelnum)]['xscale']) 
+            ax.set_yscale(datafiles['panel'+str(panelnum)]['yscale']) 
+            if datafiles['panel'+str(panelnum)]['ylim'] != []:
+                ydown = datafiles['panel'+str(panelnum)]['ylim'][0]
+                yup   = datafiles['panel'+str(panelnum)]['ylim'][1]
+                ax.set_ylim(ydown,yup)
+            if datafiles['panel'+str(panelnum)]['xlim'] != []:
+                xmin,xmax = datafiles['panel'+str(panelnum)]['xlim']
+                ax.set_xlim(xmin,xmax)
+                #ax.hlines(0,xmin,xmax,linestyle='dotted')
+            for datafilenum in range(len(datafiles['panel'+str(panelnum)]['lines'].keys())): 
+                x = datafiles['panel'+str(panelnum)]['lines'][str(datafilenum+1)]['x']
+                y = datafiles['panel'+str(panelnum)]['lines'][str(datafilenum+1)]['y']
+                #xx = datafiles['panel'+str(panelnum)]['lines'][str(datafilenum+1)]['velocity']
+                #print 'weird cores',corenames[(xx<-2)|(xx>2)]
+                #ax.hist(x,bins='auto',range=(xmin,xmax))
+                linestyle = datafiles['panel'+str(panelnum)]['lines'][str(datafilenum+1)]['linestyles']
+                legend = datafiles['panel'+str(panelnum)]['lines'][str(datafilenum+1)]['legends']
+                drawsty = datafiles['panel'+str(panelnum)]['lines'][str(datafilenum+1)]['drawsty']
+                ax.plot(x,y,linestyle,label=legend,drawstyle=drawsty)
+                #ax.text(peakvelocity+0.8, yup*0.9, '%.1f' % peakvelocity + ',' + '%.1f' % peaksnr,horizontalalignment='left',verticalalignment='center',fontsize=12)
+            ax.legend(frameon=False,prop={'size':14},labelspacing=0.2) 
+            if j == 0:
+                ax.set_title('peak')
+            ax.text(0.05, 0.9,datafiles['panel'+str(panelnum)]['title'],horizontalalignment='left',verticalalignment='center',transform = ax.transAxes)
+            #ax.text(0.1, 0.9,datafiles['panel'+str(panelnum)]['title']+' '+datafiles['panel'+str(panelnum)]['text'],horizontalalignment='left',verticalalignment='center',transform = ax.transAxes,fontsize=12)
+            #ax.text(0.95, 0.9,'('+str(cc+1)+lletter[j]+')',horizontalalignment='right',verticalalignment='center',transform = ax.transAxes,fontsize=12)
+            xlabel = datafiles['panel'+str(panelnum)]['xlabel']
+            ylabel = datafiles['panel'+str(panelnum)]['ylabel']
+            #vertlinex = datafiles['panel'+str(panelnum)]['vertlines']
+            #for vl in vertlinex:
+            #    ax.vlines(vl,ydown,yup*0.6,linestyles='dashed',colors='b')
+            if j != ypanels-1:
+                ax.set_yticks(ax.get_yticks()[1:])
+                ax.set_xticklabels(ax.get_xlabel(),visible=False)
+            else: 
+                ax.set_xlabel(xlabel)
+            if i != 0:
+                ax.set_yticklabels(ax.get_ylabel(),visible=False) 
+                ax.set_xticks(ax.get_xticks()[1:]) 
+            else: 
+                ax.set_ylabel(ylabel)
+            minor_locator = AutoMinorLocator(5)
+            ax.xaxis.set_minor_locator(minor_locator)
+            ax.tick_params(axis='both',direction='in',length=5,which='major',top=True,right=True)
+            ax.tick_params(axis='both',direction='in',length=3,which='minor',top=True,right=True)
+            
+    os.system('rm '+pdfname)
+    plt.savefig(pdfname,bbox_inches='tight')
+    plt.close(fig)
+    os.system('open '+pdfname)
+    os.system('cp '+pdfname+os.path.expandvars(' ~/GoogleDrive/imagesSFE/'))
+
+
+massmom1 = 1
+if massmom1 == 1:
+    diffvelocities = [nh3velocities-coremom1_12CO,nh3velocities-coremom1_13CO,nh3velocities-coremom1_C18O]
+    datafiles = {}
+    for i in range(0,xpanels):
+        for j in range(0,ypanels):
+            print linenames[j]
+            panel = i+j*xpanels+1
+            print 'panel',panel 
+            coreveldiff = diffvelocities[panel-1]
+            datafiles['panel'+str(panel)] = {'title':linenames[j],'lines':{'1':{'x':coreveldiff,'y':coremasses,'velocity':coreveldiff,'peaksnr':[],'legends':'data','linestyles':'ko','drawsty':'default'},},'xlim':[vlow,vhigh],'ylim':[mlow,mhigh],'xscale':'linear','yscale':'linear','xlabel':r'$v_{\rm diff}~\rm (km~s^{-1})$','ylabel':r'$\rm mass$','text':'','vertlines':[]}
+    
+    fig=plt.figure(figsize=(xpanelwidth*xpanels*1.1,ypanelwidth*ypanels/1.1))
+    plt.subplots_adjust(wspace=0.001,hspace=0.001)
+    pdfname='corespectra/vdiffmom1mass.pdf'
+    for i in range(0,xpanels):
+        for j in range(0,ypanels):
+            panelnum = i+j*xpanels+1
+            ax = fig.add_subplot(ypanels,xpanels,panelnum)
+            if 'panel'+str(panelnum) not in datafiles.keys(): continue
+            ax.set_xscale(datafiles['panel'+str(panelnum)]['xscale']) 
+            ax.set_yscale(datafiles['panel'+str(panelnum)]['yscale']) 
+            if datafiles['panel'+str(panelnum)]['ylim'] != []:
+                ydown = datafiles['panel'+str(panelnum)]['ylim'][0]
+                yup   = datafiles['panel'+str(panelnum)]['ylim'][1]
+                ax.set_ylim(ydown,yup)
+            if datafiles['panel'+str(panelnum)]['xlim'] != []:
+                xmin,xmax = datafiles['panel'+str(panelnum)]['xlim']
+                ax.set_xlim(xmin,xmax)
+                #ax.hlines(0,xmin,xmax,linestyle='dotted')
+            for datafilenum in range(len(datafiles['panel'+str(panelnum)]['lines'].keys())): 
+                x = datafiles['panel'+str(panelnum)]['lines'][str(datafilenum+1)]['x']
+                y = datafiles['panel'+str(panelnum)]['lines'][str(datafilenum+1)]['y']
+                #xx = datafiles['panel'+str(panelnum)]['lines'][str(datafilenum+1)]['velocity']
+                #print 'weird cores',corenames[(xx<-2)|(xx>2)]
+                #ax.hist(x,bins='auto',range=(xmin,xmax))
+                linestyle = datafiles['panel'+str(panelnum)]['lines'][str(datafilenum+1)]['linestyles']
+                legend = datafiles['panel'+str(panelnum)]['lines'][str(datafilenum+1)]['legends']
+                drawsty = datafiles['panel'+str(panelnum)]['lines'][str(datafilenum+1)]['drawsty']
+                ax.plot(x,y,linestyle,label=legend,drawstyle=drawsty)
+                #ax.text(peakvelocity+0.8, yup*0.9, '%.1f' % peakvelocity + ',' + '%.1f' % peaksnr,horizontalalignment='left',verticalalignment='center',fontsize=12)
+            ax.legend(frameon=False,prop={'size':14},labelspacing=0.2) 
+            if j == 0:
+                ax.set_title('mom1')
+            ax.text(0.05, 0.9,datafiles['panel'+str(panelnum)]['title'],horizontalalignment='left',verticalalignment='center',transform = ax.transAxes)
+            #ax.text(0.1, 0.9,datafiles['panel'+str(panelnum)]['title']+' '+datafiles['panel'+str(panelnum)]['text'],horizontalalignment='left',verticalalignment='center',transform = ax.transAxes,fontsize=12)
+            #ax.text(0.95, 0.9,'('+str(cc+1)+lletter[j]+')',horizontalalignment='right',verticalalignment='center',transform = ax.transAxes,fontsize=12)
+            xlabel = datafiles['panel'+str(panelnum)]['xlabel']
+            ylabel = datafiles['panel'+str(panelnum)]['ylabel']
+            #vertlinex = datafiles['panel'+str(panelnum)]['vertlines']
+            #for vl in vertlinex:
+            #    ax.vlines(vl,ydown,yup*0.6,linestyles='dashed',colors='b')
+            if j != ypanels-1:
+                ax.set_yticks(ax.get_yticks()[1:])
+                ax.set_xticklabels(ax.get_xlabel(),visible=False)
+            else: 
+                ax.set_xlabel(xlabel)
+            if i != 0:
+                ax.set_yticklabels(ax.get_ylabel(),visible=False) 
+                ax.set_xticks(ax.get_xticks()[1:]) 
+            else: 
+                ax.set_ylabel(ylabel)
+            minor_locator = AutoMinorLocator(5)
+            ax.xaxis.set_minor_locator(minor_locator)
+            ax.tick_params(axis='both',direction='in',length=5,which='major',top=True,right=True)
+            ax.tick_params(axis='both',direction='in',length=3,which='minor',top=True,right=True)
+            
+    os.system('rm '+pdfname)
+    plt.savefig(pdfname,bbox_inches='tight')
+    plt.close(fig)
+    os.system('open '+pdfname)
+    os.system('cp '+pdfname+os.path.expandvars(' ~/GoogleDrive/imagesSFE/'))
 
 
