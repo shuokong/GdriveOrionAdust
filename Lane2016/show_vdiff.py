25797c33d7eefba34132b22a51e8b683d98fdfa1
Shuo Kong <shuo.kong@yale.edu>
2018-06-21 09:40:38 -0400

    add sigma histograms

--- Lane2016/show_vdiff.py
+++ Lane2016/show_vdiff.py
@@ -44,10 +44,11 @@ cols = len(corenames)
 
 usepeakvel = 0
 usemom1vel = 0
-usegausvel = 1
+usegausvel = 0
 massvel = 0
 massmom1 = 0
 tkinhist = 0
+gaussigmahist = 1
 
 if usepeakvel == 1:
     diffvelocities = [nh3velocities-corevelocities12CO,nh3velocities-corevelocities13CO,nh3velocities-corevelocitiesC18O]
@@ -580,6 +581,96 @@ if tkinhist == 1:
             ax.xaxis.set_minor_locator(minor_locator)
             ax.tick_params(axis='both',direction='in',length=5,which='major',top=True,right=True)
             ax.tick_params(axis='both',direction='in',length=3,which='minor',top=True,right=True)
+            
+    os.system('rm '+pdfname)
+    plt.savefig(pdfname,bbox_inches='tight')
+    plt.close(fig)
+    os.system('open '+pdfname)
+    os.system('cp '+pdfname+os.path.expandvars(' ~/GoogleDrive/imagesSFE/'))
+
+if gaussigmahist == 1:
+    gaussigmas = [coregaus_sigma_C18O,coregaus_sigma_13CO,coregaus_sigma_C18O] # first element dummy
+    mom2s = [coremom2_C18O,coremom2_13CO,coremom2_C18O] # first element dummy
+    #print 'nh3velocities',nh3velocities
+    removeind = (coregaus_sigma_13CO<=0.)|np.isnan(coregaus_sigma_13CO)|(coregaus_sigma_C18O<=0.)|np.isnan(coregaus_sigma_C18O)|(coremom2_13CO<=0.)|np.isnan(coremom2_13CO)|(coremom2_C18O<=0.)|np.isnan(coremom2_C18O)
+    #print 'removeind',removeind
+    #sys.exit()
+    datafiles = {}
+    vlow = 0
+    vhigh = 2
+    for i in range(0,xpanels):
+        for j in range(1,ypanels): # start from 13CO
+            print linenames[j]
+            panel = i+j*xpanels+1
+            print 'panel',panel 
+            coreveldiff = gaussigmas[panel-1][~removeind]
+            print 'len(coreveldiff)',len(coreveldiff)
+            print 'min max coreveldiff',min(coreveldiff),max(coreveldiff)
+            hist, bin_edges = np.histogram(coreveldiff,bins='auto',range=(vlow,vhigh))
+            bincenter = (bin_edges[:-1] + bin_edges[1:]) / 2.
+            datafiles['panel'+str(panel-1)] = {'title':linenames[j],'lines':{'1':{'x':bincenter,'y':hist,'velocity':coreveldiff,'peaksnr':[],'legends':'Gauss','linestyles':'k-','drawsty':'steps-mid'},},'xlim':[vlow,vhigh],'ylim':[0,50],'xscale':'linear','yscale':'linear','xlabel':r'$\sigma~\rm (km~s^{-1})$','ylabel':r'$\rm number$','text':'','vertlines':[0.31]}
+            coreveldiff = mom2s[panel-1][~removeind]
+            print 'len(coreveldiff)',len(coreveldiff)
+            hist, bin_edges = np.histogram(coreveldiff,bins='auto',range=(vlow,vhigh))
+            bincenter = (bin_edges[:-1] + bin_edges[1:]) / 2.
+            popt,pcov = curve_fit(gaus,bincenter,hist,p0=[1,0,0.5])
+            datafiles['panel'+str(panel-1)]['lines']['2'] = {'x':bincenter,'y':hist,'velocity':coreveldiff,'peaksnr':[],'legends':'2nd moment','linestyles':'b-','drawsty':'steps-mid'}
+    
+    fig=plt.figure(figsize=(xpanelwidth*xpanels*1.1,ypanelwidth*ypanels/1.1))
+    plt.subplots_adjust(wspace=0.001,hspace=0.001)
+    pdfname='corespectra/sigmahist.pdf'
+    ypanels = 2
+    for i in range(0,xpanels):
+        for j in range(0,ypanels):
+            panelnum = i+j*xpanels+1
+            ax = fig.add_subplot(ypanels,xpanels,panelnum)
+            if 'panel'+str(panelnum) not in datafiles.keys(): continue
+            #ax.set_xscale(datafiles['panel'+str(panelnum)]['xscale']) 
+            #ax.set_yscale(datafiles['panel'+str(panelnum)]['yscale']) 
+            if datafiles['panel'+str(panelnum)]['ylim'] != []:
+                ydown = datafiles['panel'+str(panelnum)]['ylim'][0]
+                yup   = datafiles['panel'+str(panelnum)]['ylim'][1]
+                ax.set_ylim(ydown,yup)
+            if datafiles['panel'+str(panelnum)]['xlim'] != []:
+                xmin,xmax = datafiles['panel'+str(panelnum)]['xlim']
+                ax.set_xlim(xmin,xmax)
+                #ax.hlines(0,xmin,xmax,linestyle='dotted')
+            for datafilenum in range(len(datafiles['panel'+str(panelnum)]['lines'].keys())): 
+                x = datafiles['panel'+str(panelnum)]['lines'][str(datafilenum+1)]['x']
+                y = datafiles['panel'+str(panelnum)]['lines'][str(datafilenum+1)]['y']
+                #xx = datafiles['panel'+str(panelnum)]['lines'][str(datafilenum+1)]['velocity']
+                #print 'weird cores',corenames[(xx<-2)|(xx>2)]
+                #ax.hist(x,bins='auto',range=(xmin,xmax))
+                linestyle = datafiles['panel'+str(panelnum)]['lines'][str(datafilenum+1)]['linestyles']
+                legend = datafiles['panel'+str(panelnum)]['lines'][str(datafilenum+1)]['legends']
+                drawsty = datafiles['panel'+str(panelnum)]['lines'][str(datafilenum+1)]['drawsty']
+                ax.plot(x,y,linestyle,label=legend,drawstyle=drawsty)
+                #ax.text(peakvelocity+0.8, yup*0.9, '%.1f' % peakvelocity + ',' + '%.1f' % peaksnr,horizontalalignment='left',verticalalignment='center',fontsize=12)
+            ax.legend(frameon=False,prop={'size':14},labelspacing=0.2) 
+            if j == 0:
+                ax.set_title('Gauss')
+            ax.text(0.05, 0.9,datafiles['panel'+str(panelnum)]['title'],horizontalalignment='left',verticalalignment='center',transform = ax.transAxes)
+            #ax.text(0.1, 0.9,datafiles['panel'+str(panelnum)]['title']+' '+datafiles['panel'+str(panelnum)]['text'],horizontalalignment='left',verticalalignment='center',transform = ax.transAxes,fontsize=12)
+            #ax.text(0.95, 0.9,'('+str(cc+1)+lletter[j]+')',horizontalalignment='right',verticalalignment='center',transform = ax.transAxes,fontsize=12)
+            xlabel = datafiles['panel'+str(panelnum)]['xlabel']
+            ylabel = datafiles['panel'+str(panelnum)]['ylabel']
+            vertlinex = datafiles['panel'+str(panelnum)]['vertlines']
+            for vl in vertlinex:
+                ax.vlines(vl,ydown,yup*0.7,linestyles='dotted',colors='k')
+            if j != ypanels-1:
+                ax.set_yticks(ax.get_yticks()[1:])
+                ax.set_xticklabels(ax.get_xlabel(),visible=False)
+            else: 
+                ax.set_xlabel(xlabel)
+            if i != 0:
+                ax.set_yticklabels(ax.get_ylabel(),visible=False) 
+                ax.set_xticks(ax.get_xticks()[1:]) 
+            else: 
+                ax.set_ylabel(ylabel)
+            minor_locator = AutoMinorLocator(5)
+            ax.xaxis.set_minor_locator(minor_locator)
+            ax.tick_params(axis='both',direction='in',length=5,which='major',top=True,right=True)
+            ax.tick_params(axis='both',direction='in',length=2,which='minor',top=True,right=True)
             
     os.system('rm '+pdfname)
     plt.savefig(pdfname,bbox_inches='tight')
